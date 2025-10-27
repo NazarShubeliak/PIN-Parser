61682f9047d155f9e5a9e4703fd477c1559c5c56
Nazar <test@gmail.com>
2025-10-27 14:22:42 +0200

    feat(sheet): add simple SheetService class for cell updates; fix minor bugs in parser

--- parser/pipeline.py
+++ parser/pipeline.py
@@ -1,8 +1,8 @@
-
-from parser.config import logger, DOWNLOAD_DIR
+from parser.config import logger, DOWNLOAD_DIR, GOOGLE_SHEET_NAME, GOOGLE_SHEET_WORKSHEET_NAME
 from parser.api_client import fetch_documents, download_all_documents
 from parser.tools import clear_folder, find_document
 from parser.pdf_parser import run_parser
+from parser.sheet import SheetService
 
 def run_pipeline(start_date: str, end_date: str) -> None:
     """Run the full PIN parsing pipeline"""
@@ -16,10 +16,14 @@ def run_pipeline(start_date: str, end_date: str) -> None:
     documents = fetch_documents(start_date, end_date)
     logger.info(f"Found {len(documents)} documents")
 
-    # Step 3: Download all documents
+    # Step 3: Download documents
     download_all_documents(documents, DOWNLOAD_DIR)
     logger.info(f"Download all documents to {DOWNLOAD_DIR}")
 
     # Step 4: Parsing document
     filename = find_document(DOWNLOAD_DIR)
     german, non_europe, europe = run_parser(filename) # German, NON-Europe, Europe
+
+    # Step 5: Add data to Google Sheet
+    sheet_service = SheetService(GOOGLE_SHEET_NAME)
+    sheet_service.append_rows(GOOGLE_SHEET_WORKSHEET_NAME, [german, non_europe, europe])
---
+++ parser/sheet.py
@@ -0,0 +1,53 @@
+import gspread
+from parser.config import logger, GOOGLE_TOKEN
+from oauth2client.service_account import ServiceAccountCredentials
+
+
+class SheetService:
+    def __init__(self, sheet_name: str) -> None:
+        self.client = self.__authorize()
+        self.sheet = self.client.open(sheet_name)
+
+    def __authorize(self) -> gspread.Client:
+        """
+        Authorizes and returns a gspread client using service account credentials.
+
+        Returns:
+            gspread.Client: Authorized client for interacting with Google Sheets.
+        """
+        scope = [
+            "https://spreadsheets.google.com/feeds",
+            "https://www.googleapis.com/auth/drive",
+        ]
+        creds = ServiceAccountCredentials.from_json_keyfile_name(GOOGLE_TOKEN, scope)
+        client = gspread.authorize(creds)
+        logger.debug("Authorize to Google Service complete")
+
+        return client
+
+    def get_worksheet(self, worksheet_name: str) -> gspread.Worksheet:
+        """
+        Returns a specific worksheet by name.
+
+        Args:
+            name (str): The name of the worksheet tab.
+
+        Returns:
+            gspread.Worksheet: The worksheet object.
+        """
+        return self.sheet.worksheet(worksheet_name)
+
+    def append_rows(self, worksheet_name: str, rows: list[float, float, float]) -> None:
+        """
+        Updates specific cells in the worksheet with provided values.
+
+        Args:
+            worksheet_name (str): Name of the worksheet to update.
+            rows (list[float]): List of three floats: [EUROPE, NON-EUROPE, GERMAN]
+        """
+        if len(rows) != 3:
+            raise ValueError("Expected exatly 3 value")
+        ws = self.get_worksheet(worksheet_name)
+        ws.update("G21", [[rows[0]]])
+        ws.update("F21", [[rows[1]]])
+        ws.update("D21", [[rows[2]]])
